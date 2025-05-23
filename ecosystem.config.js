8462a5ff5f2efce3e0cd179ee336eb4a0b3bc7a8
nguyenthanhdat <nguyend@digitalplatformer.co.j>
2025-05-23 16:54:38 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: 'my-akat',
-      script: '/var/www/akat-BE/dist/index.js',
+      name: 'fb-rent-account',
+      script: '/var/www/fb-rent-account/dist/index.js',
       interpreter: '/root/.nvm/versions/node/v20.16.0/bin/node', // Đảm bảo sử dụng đúng Node.js từ NVM
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
-      env_file: '/var/www/akat-BE/.env',
+      env_file: '/var/www/fb-rent-account/.env',
       env: {
         NODE_ENV: 'development',
       },
