0b9c95814bc08b834ff3c9822a7421c0801287c0
hungnv <hungnv@teracom.vn>
2025-06-10 15:27:19 +0700

    [feat] api statistic monthly

--- src/config/redis-config.ts
+++ src/config/redis-config.ts
@@ -1,4 +1,5 @@
 import Redis from 'ioredis';
+import 'dotenv/config';
 
 const redisClient = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
--- src/routes/statistics.routes.ts
+++ src/routes/statistics.routes.ts
@@ -2,4 +2,5 @@ import express from 'express';
 import statisticsController from '../controllers/statistics.controller';
 const router = express.Router();
 router.get('/statistics', statisticsController.getStatistics);
+router.get('/monthlyStatistics', statisticsController.getStatisticsMonthly);
 export default router;
