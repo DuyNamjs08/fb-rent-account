78b6bf91c0d34811542b919dbe578d5291af8142
nguyenthanhdat <nguyend@digitalplatformer.co.j>
2025-06-23 15:34:05 +0700

    fix-v2

--- src/controllers/adAccount.controller.ts
+++ src/controllers/adAccount.controller.ts
@@ -382,7 +382,7 @@ const TKQCController = {
       const pageSizeNum = Number(pageSize) || 10;
       const result = await prisma.adsAccount.findMany({
         where: {
-          is_visa_account: false || null,
+          OR: [{ is_visa_account: false }, { is_visa_account: null }],
         },
         skip,
         take: pageSizeNum,
